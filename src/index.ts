27527eda6b7dc226234491031264c120f304789c
FarrazID2 <farraz02@gmail.com>
2024-09-04 17:17:24 +0700

    Test Error script - by adding **

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
-
+**
 // Endpoint untuk register user
 app.post('/register', (req: Request, res: Response) => {
   const { username, password } = req.body;
